412ff843f728789a33ed1ff59eda7ca2d46d4794
k8188219 <k8188219@gmail.com>
2024-09-22 06:34:00 +0000

    chore: remove files

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Redirect } from "expo-router";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Redirect href="/greeting"/>
+    </View>
+  );
+}
